185f18ae81c596f319e58cd238cb47785870f835
Paulo Zanluqui <paulozanluqui13@gmail.com>
2024-06-27 00:22:24 -0300

    Initial commit

---
+++ Context/OrganizadorContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using dotnet_api_desafio.Models;
+
+namespace dotnet_api_desafio.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ Models/EnumStatusTarefa.cs
@@ -0,0 +1,8 @@
+namespace dotnet_api_desafio.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado
+    }
+}
